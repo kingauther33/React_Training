42918685a539dfc318f31764edcf9eb7910801b2
An <kingauther33@gmail.com>
2021-07-03 18:06:50 +0700

    Update Training Code

--- react-router/course-directory/src/components/App.js
+++ react-router/course-directory/src/components/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Route
-} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, HashRouter, Route, Switch } from "react-router-dom";
 
 // App components
 import Home from "./Home";
-import About from './About';
-import Header from './Header';
-import Teachers from './Teachers';
+import About from "./About";
+import Header from "./Header";
+import Teachers from "./Teachers";
+import Courses from "./Courses";
+import NotFound from './NotFound';
+import Featured from './Featured';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/about">
-        <About />
-      </Route>
-      <Route exact path="/teacher">
-        <Teachers />
-      </Route>
-      <Route exact path="/courses">
-        <About />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About title="About" />
+        </Route>
+        <Route exact path="/teachers" component={Teachers} />
+        <Route path="/teachers/:topic?/:name" component={Featured}/>
+        <Route path="/courses" component={Courses} />
+        {/* <Route exact path="/courses" component={Courses}>
+      <Route exact path="/courses" render={ () => <Courses title='About'/>}> */}
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
--- react-router/course-directory/src/components/Courses.js
+++ react-router/course-directory/src/components/Courses.js
@@ -1,18 +1,50 @@
 import React from 'react';
+import { Route, NavLink, Redirect, useRouteMatch } from "react-router-dom";
 
-const Courses = () => (
-  <div className="main-content courses">
-    <div className="course-header group">
-      <h2>Courses</h2> 
-      <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
-      </ul>
+import CourseContainer from './courses/CourseContainer';
+import { HTMLCourses, CSSCourses, JSCourses } from "../data/courses";
+
+const Courses = () => {
+  let match = useRouteMatch();
+
+  return (
+    <div className="main-content courses">
+      <div className="course-header group">
+        <h2>Courses</h2>
+        <ul className="course-nav">
+          <li>
+            <NavLink to={`${match.url}/html`}>HTML</NavLink>
+          </li>
+          <li>
+            <NavLink to={`${match.url}/css`}>CSS</NavLink>
+          </li>
+          <li>
+            <NavLink to={`${match.url}/javascript`}>JavaScript</NavLink>
+          </li>
+        </ul>
+      </div>
+
+      {/* Write routes here... */}
+      <Route exact path={match.path}>
+        <Redirect to={`${match.path}/html`} />
+      </Route>
+      {/* <Route
+        path={`${match.path}/css`}
+        render={() => <CourseContainer data={CSSCourses} />}
+      /> */}
+      <Route path={`${match.path}/css`}>
+        <CourseContainer data={CSSCourses} />
+      </Route>
+      <Route
+        path={`${match.path}/html`}
+        render={() => <CourseContainer data={HTMLCourses} />}
+      />
+      <Route
+        path={`${match.path}/javascript`}
+        render={() => <CourseContainer data={JSCourses} />}
+      />
     </div>
-    
-    {/* Write routes here... */}
-  </div>
-);
+  );
+};
 
 export default Courses;
