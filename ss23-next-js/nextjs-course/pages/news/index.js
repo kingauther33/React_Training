daa012dbf367110bd361489e1ced354e572d3f2d
kingauther33 <kingauther33@gmail.com>
2022-01-18 23:35:48 +0700

    Update ss23 next-js

---
+++ ss23-next-js/nextjs-course/pages/news/index.js
@@ -0,0 +1,20 @@
+// domain.com/news
+import Link from 'next/link';
+
+function NewsPage() {
+	return (
+		<>
+			<h1>The News Page</h1>
+			<ul>
+				<li>
+					<Link href="/news/nextjs-is-a-great-framework">
+						NextJs is a great framework
+					</Link>
+				</li>
+				<li>Something else</li>
+			</ul>
+		</>
+	);
+}
+
+export default NewsPage;
