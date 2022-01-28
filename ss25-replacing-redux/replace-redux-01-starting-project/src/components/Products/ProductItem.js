71adb884e2f589023b18a58495c862cbb64a4eb4
kingauther33 <kingauther33@gmail.com>
2022-01-28 21:24:09 +0700

    Update ss25

--- ss25-replacing-redux/replace-redux-01-starting-project/src/components/Products/ProductItem.js
+++ ss25-replacing-redux/replace-redux-01-starting-project/src/components/Products/ProductItem.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useContext } from 'react';
+import { useStore } from '../../hooks-store/store';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
 
-const ProductItem = props => {
-  const dispatch = useDispatch();
+// import { ProductsContext } from './../../context/products-context';
 
-  const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
-  };
+const ProductItem = React.memo((props) => {
+	const dispatch = useStore(false)[1];
 
-  return (
-    <Card style={{ marginBottom: '1rem' }}>
-      <div className="product-item">
-        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
-        <p>{props.description}</p>
-        <button
-          className={!props.isFav ? 'button-outline' : ''}
-          onClick={toggleFavHandler}
-        >
-          {props.isFav ? 'Un-Favorite' : 'Favorite'}
-        </button>
-      </div>
-    </Card>
-  );
-};
+	console.log('Rendering');
+	// const toggleFav = useContext(ProductsContext).toggleFav;
+
+	const toggleFavHandler = () => {
+		dispatch('TOGGLE_FAV', props.id);
+		// toggleFav(props.id);
+	};
+
+	return (
+		<Card style={{ marginBottom: '1rem' }}>
+			<div className="product-item">
+				<h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+				<p>{props.description}</p>
+				<button
+					className={!props.isFav ? 'button-outline' : ''}
+					onClick={toggleFavHandler}
+				>
+					{props.isFav ? 'Un-Favorite' : 'Favorite'}
+				</button>
+			</div>
+		</Card>
+	);
+});
 
 export default ProductItem;
